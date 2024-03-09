54556dadbbfc7975758c5475b66ae1b24a498343
Arnold-In-Tech <g16a7782@gmail.com>
2024-03-09 15:54:05 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INT,
+    name VARCHAR(1000),
+    age INT,
+    sex VARCHAR(10),
+    color VARCHAR(20),
+    temperament VARCHAR(100),
+    alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+            VALUES (1, "Mr. Chocolate", 7, "M", "black", "hot", TRUE),
+                    (2, "Rowdy", 7, "M", "yellow", "hot", TRUE),
+                    (3, "Tabitha", 7, "M", "white", "mild", FALSE),
+                    (4, "Sergeant Brown", 7, "M", "brown", "cool", TRUE),
+                    (5, "Melissa", 7, "M", "yellow", "mild", TRUE),
+                    (6, "Grinch", 7, "M", "green", "hot", TRUE),
+                    (7, "Wendy", 7, "M", "red", "mild", TRUE),
+                    (8, NULL, NULL, NULL, NULL, NULL, NULL);
